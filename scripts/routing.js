ce37b55264d23a0f8102ed56996895cea14b862e
Stephen Eggering <seggering@hunter.com>
2019-12-06 11:01:19 -0600

    quick

--- scripts/routing.js
+++ scripts/routing.js
@@ -1,23 +1,12 @@
-
-    $(document).ready(function () {
-        var str = window.location + '';
-        var query = str.split('?')[1];
-        var queries;
-        var pairs = {};
-        if (query != null) {
-            queries = query.split('&');
-            queries.forEach(function (element) {
-                pairs[element.split('=')[0]] = element.split('=')[1];
-            });
-        }
-        var location = pairs["place"];
-        if (location == "here") {
-            document.getElementById("body").innerText = "Here";
-        }
-        else if (location == "there") {
-            document.getElementById("body").innerText = "There";
-        }
-        else {
-            document.getElementById("body").innerText = "Home";
-        }
-    })();
+var pairs = {};
+(function() {
+  var str = window.location + "";
+  var query = str.split("?")[1];
+  var queries;
+  if (query != null) {
+    queries = query.split("&");
+    queries.forEach(function(element) {
+      pairs[element.split("=")[0]] = element.split("=")[1];
+    });
+  }
+})();
