6c86bf21c65ea61576f5fcda7469ffce13b96474
kepanoegg <stephen.eggering@gmail.com>
2021-01-21 09:45:07 -0600

    quick

---
+++ giflist.js
@@ -0,0 +1,12 @@
+
+var jsonlist = [
+    "https://media2.giphy.com/media/BOp4FhVQmGRbRtAvwH/giphy.gif",
+    "https://media4.giphy.com/media/y7g67j9snOjmapWYzK/giphy.gif",
+    "https://media4.giphy.com/media/6u1R3Z6jQGk7SHy0Nl/giphy.gif",
+    "https://media3.giphy.com/media/Lm6YoNJsMQSvvgNexZ/giphy.gif",
+    "https://media2.giphy.com/media/jti7DMOcHkS6MOclaD/giphy.gif",
+    "https://media3.giphy.com/media/KB8NidqL600GFWiCAR/giphy.gif",
+    "https://media0.giphy.com/media/m9eG1qVjvN56H0MXt8/giphy.gif",
+    "https://media3.giphy.com/media/jIquFF9n3kZC4GVHAo/giphy.gif"
+
+];
