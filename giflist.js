11b10a3cafe0c998db48f3b15a05fab7249f7bb6
kepanoegg <stephen.eggering@gmail.com>
2021-01-22 07:39:20 -0600

    quick

--- giflist.js
+++ giflist.js
@@ -10,5 +10,6 @@ var jsonlist = [
     "https://media3.giphy.com/media/jIquFF9n3kZC4GVHAo/giphy.gif",
     "https://media0.giphy.com/media/Bb10VgS69zzhKpOx7J/giphy.gif",
     "https://media3.giphy.com/media/fHaMJCQBsFeOKQy6xW/giphy.gif",
-    "https://media1.tenor.com/images/d8537c7884066a8a098ba8b37191dcc2/tenor.gif"
+    "https://media1.tenor.com/images/d8537c7884066a8a098ba8b37191dcc2/tenor.gif",
+    "https://thumbs.gfycat.com/WhiteCheerfulGroundhog-small.gif"
 ];
