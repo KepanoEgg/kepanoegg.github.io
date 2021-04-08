0a425668a73f9aa62b45563088ed31510389a2ad
kepanoegg <stephen.eggering@gmail.com>
2021-04-08 08:18:59 -0500

    quick

---
+++ giflist2.js
@@ -0,0 +1,139 @@
+
+var jsonlist = [
+    {
+        "url": "https://media2.giphy.com/media/BOp4FhVQmGRbRtAvwH/giphy.gif",
+        "tags": "bobs"
+    },
+    {
+        "url": "https://media4.giphy.com/media/y7g67j9snOjmapWYzK/giphy.gif",
+        "tags": "bobs"
+    },
+    {
+        "url": "https://media4.giphy.com/media/6u1R3Z6jQGk7SHy0Nl/giphy.gif",
+        "tags": "trippy"
+    },
+    {
+        "url": "https://media3.giphy.com/media/Lm6YoNJsMQSvvgNexZ/giphy.gif",
+        "tags": "hunter,bye"
+    },
+    {
+        "url": "https://media2.giphy.com/media/jti7DMOcHkS6MOclaD/giphy.gif",
+        "tags": "hunter,bye"
+    },
+    {
+        "url": "https://media3.giphy.com/media/KB8NidqL600GFWiCAR/giphy.gif",
+        "tags": "bye"
+    },
+    {
+        "url": "https://media0.giphy.com/media/m9eG1qVjvN56H0MXt8/giphy.gif",
+        "tags": "bye"
+    },
+    {
+        "url": "https://media3.giphy.com/media/jIquFF9n3kZC4GVHAo/giphy.gif",
+        "tags": "hi,hello"
+    },
+    {
+        "url": "https://media0.giphy.com/media/Bb10VgS69zzhKpOx7J/giphy.gif",
+        "tags": "stretch"
+    },
+    {
+        "url": "https://media3.giphy.com/media/fHaMJCQBsFeOKQy6xW/giphy.gif",
+        "tags": "bernie"
+    },
+    {
+        "url": "https://media1.tenor.com/images/d8537c7884066a8a098ba8b37191dcc2/tenor.gif",
+        "tags": "no"
+    },
+    {
+        "url": "https://thumbs.gfycat.com/WhiteCheerfulGroundhog-small.gif",
+        "tags": "friday,friends,hi"
+    },
+    {
+        "url":"https://thumbs.gfycat.com/ConcreteLinearIrishsetter-small.gif",
+        "tags": "crazy"
+    },
+    {
+        "url": "https://media1.giphy.com/media/lmQ4771HAEc1O/giphy.gif?cid=ecf05e472fb0b1d55aeebca3af7d65ca0297f442ecf22f07&rid=giphy.gif",
+        "tags": "bye,weekend"
+    },
+    {
+        "url":"https://media4.giphy.com/media/3ufEDmuO5vV3IvWdj0/giphy.gif",
+        "tags": "bobs,drugs"
+    },
+    {
+        "url": "https://media1.giphy.com/media/bFAtZzfgUZ043jIwK2/giphy.gif",
+        "tags": "hi"
+    },
+    {
+        "url":"https://media3.giphy.com/media/yNuRHLW08IJeCG6NGH/giphy.gif",
+        "tags": "bye"
+    },
+    {
+        "url":"https://y.yarn.co/77810eec-e0dc-44a3-aa1f-d99d05ed7941_text_hi.gif",
+        "tags": "office"
+    },
+    {
+        "url": "https://media.tenor.com/images/6a9f9b6b9d2b5cabc7fe17f081d5c960/tenor.gif",
+        "tags": "rug"
+    },
+    {
+        "url": "https://media4.giphy.com/media/1qhGVJLBXdAuURTvGg/giphy.gif",
+        "tags": "emily"
+    },
+    {
+        "url": "https://media4.giphy.com/media/NTU88q0bnsXYvmRZcc/giphy.gif",
+        "tags": "emily"
+    },
+    {
+        "url": "https://media0.giphy.com/media/wII4Of9CNKfWnVLimx/giphy.gif",
+        "tags": "emily,hi"
+    },
+    {
+        "url": "https://media3.giphy.com/media/m9rymi3wz8O352yixI/giphy.gif",
+        "tags": "emily"
+    },
+    {
+        "url": "https://media3.giphy.com/media/8w3tUOSluv8ojoA7XJ/giphy.gif",
+        "tags": "emily"
+    },
+    {
+        "url": "https://media4.giphy.com/media/1QdTbuzeVnpjbCyyDI/giphy.gif",
+        "tags": "emily"
+    },
+    {
+        "url": "https://media3.giphy.com/media/4WEKNdHissROu0aVaH/giphy.gif",
+        "tags": "emily,magic"
+    },
+    {
+        "url": "https://media2.giphy.com/media/9V3I9bpMVSIzVpn88a/giphy.gif",
+        "tags": "emily,hi,hello,hey"
+    },
+    {
+        "url": "https://media2.giphy.com/media/8YR0i5L5j1g7sTsMJQ/giphy.gif",
+        "tags": "emily"
+    },
+    {
+        "url": "https://media2.giphy.com/media/SKzaGBdQ7DjOpf7zIe/giphy.gif",
+        "tags": "emily,friends"
+    },
+    {
+        "url": "https://media4.giphy.com/media/8FV549WVW6gRPPl5EP/giphy.gif",
+        "tags": "emily,hey,hi,hello"
+    },
+    {
+        "url": "https://media1.giphy.com/media/YknvuKYe2xOyEl9PWP/giphy.gif",
+        "tags": "emily"
+    },
+    {
+        "url": "https://media2.giphy.com/media/AFJeDRo4g7jxUQTCq9/giphy.gif",
+        "tags": "emily"
+    },
+    {
+        "url": "https://media3.giphy.com/media/ZNyU5FQ1y5jUOh8Xhz/giphy.gif",
+        "tags": "emily"
+    },
+    {
+        "url": "https://media4.giphy.com/media/jWlUWL39RIT52OA5J1/giphy.gif",
+        "tags": "emily"
+    }
+];
