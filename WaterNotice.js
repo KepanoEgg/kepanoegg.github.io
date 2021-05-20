a4976881b16224ff994b74bb44020cf8f99de0ae
Stephen Eggering <seggering@hunter.com>
2021-05-20 14:45:07 -0500

    quick

--- WaterNotice.js
+++ WaterNotice.js
@@ -10,17 +10,10 @@ function checkNotificationPermissions() {
 }
 
 function waterNotice(amount, behind) {
-    alert(amount + "::" + behind)
+    var realBehind = 0;
+    if(behind > 0){realBehind = behind;}
   var notify = new Notification("WATER", {
-    body: "You need to drink " + amount + "oz of water. \n You are " + behind + "oz behind.",
+    body: "You need to drink " + amount + "oz of water. \n You are " + realBehind + "oz behind.",
     icon: "https://www.kepanoegg.com/images/puertoheadbob.gif",
   });
 }
-
-javascript: (function () {
-    var lotElement = null;
-    var spans = document.getElementsByTagName("span");
-    var spansList = Array.prototype.slice.call(spans);
-    spansList.forEach(function(element){if(element.innerText == "Lot:"){lotElement = element}});
-    alert(lotElement.parentNode.childNodes[2].innerText);
-})();
